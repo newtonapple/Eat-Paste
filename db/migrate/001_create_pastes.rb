0f3749b87162f5b08feacfc34d622e45c4f9e94f
David Dai <david@affinitylabs.com>
2009-05-11 01:16:59 -0700

    Adding a slim version (smaller syntax) of ultraviolet.

--- db/migrate/001_create_pastes.rb
+++ db/migrate/001_create_pastes.rb
@@ -1,6 +1,7 @@
 class CreatePastes < ActiveRecord::Migration
   def self.up
     create_table :pastes do |t|
+      t.string :default_language, :limit => 50, :default => 'plain_text'
       t.text :body, :default => ''
       t.timestamps
     end
