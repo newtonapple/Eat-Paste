f8e2bdf64b5c973fee487ae74b1e56add9a30200
David Dai <david@affinitylabs.com>
2009-05-18 20:33:25 -0700

    Use mediumtext for pastes.body. (MySQL specific)

--- db/migrate/001_create_pastes.rb
+++ db/migrate/001_create_pastes.rb
@@ -1,12 +1,17 @@
 class CreatePastes < ActiveRecord::Migration
   def self.up
-    create_table :pastes, :options => 'ENGINE=MyISAM' do |t|
-      t.string :default_language, :limit => 50, :default => 'plain_text'
-      t.text :body, :default => ''
-      t.text :preview
-      t.timestamps
-    end
-    execute 'CREATE fulltext INDEX fulltext_idx_pastes_body ON pastes(body);'
+    execute "
+      CREATE TABLE `pastes` (
+        `id` int(11) NOT NULL auto_increment,
+        `default_language` varchar(50) default 'plain_text',
+        `body` mediumtext,
+        `preview` text,
+        `created_at` datetime default NULL,
+        `updated_at` datetime default NULL,
+        PRIMARY KEY  (`id`),
+        FULLTEXT KEY `fulltext_index_on_pastes_body` (`body`)
+      ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+    ".squish
   end
 
   def self.down
